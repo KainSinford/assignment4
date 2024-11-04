9b64a0a0a8f340a77963aec772e38aefb590b24f
KainSinford <154439341+KainSinford@users.noreply.github.com>
2024-11-04 22:16:58 +0700

    Add files via upload

---
+++ TongCacChuSoMotSoNguyen.java
@@ -0,0 +1,18 @@
+package tong.cac.chu.so.mot.so.nguyen;
+import java.util.Scanner;
+public class TongCacChuSoMotSoNguyen {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);   
+
+        System.out.print("Nhap vao mot so nguyen: ");
+        int n = scanner.nextInt();   
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit;
+            n /= 10;
+        }
+        System.out.println("Tong cac chu so: " + sum);
+    }
+}
